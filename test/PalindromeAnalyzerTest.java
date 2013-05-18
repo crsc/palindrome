da217a4db55181d85ab026361ff6069466081855
Crstn <Crstn@192.168.1.36>
2013-05-18 11:14:39 +0100

    more tests for isPalindrome

--- test/PalindromeAnalyzerTest.java
+++ test/PalindromeAnalyzerTest.java
@@ -8,11 +8,13 @@ public class PalindromeAnalyzerTest {
     public void isPalindrome() {
         PalindromeAnalyzer palindrome = new PalindromeAnalyzer();
         assertEquals(true, palindrome.isPalindrome(1234321));
+        assertEquals(true, palindrome.isPalindrome(567765));
     }
     
     @Test
     public void isNotPalindrome(){
         PalindromeAnalyzer palindrome = new PalindromeAnalyzer();
         assertEquals(false, palindrome.isPalindrome(1234));
+        assertEquals(false, palindrome.isPalindrome(0121));
     }
 }
