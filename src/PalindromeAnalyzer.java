5c3fd9eb487933664a010ab8a8394cc703c9b780
Crstn <Crstn@192.168.1.36>
2013-05-18 11:00:41 +0100

    isNotPalindrome

--- src/PalindromeAnalyzer.java
+++ src/PalindromeAnalyzer.java
@@ -1,6 +1,17 @@
 public class PalindromeAnalyzer {
     
     public Boolean isPalindrome(int number){
+        String string = Integer.toString(number);
+        int[] digits = new int[string.length()];
+
+        for(int i = 0; i<string.length(); ++i){
+            digits[i] = Integer.parseInt(string.substring(i, i+1));
+        }
+        for(int i=0; i < digits.length/2; i++){
+            if(digits[i]!= digits[digits.length -i -1]){
+                return false;
+            }
+        }
         return true;
     }
     
