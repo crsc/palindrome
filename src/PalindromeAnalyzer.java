3cefe925671c521543aa1b6de2f4807a234f7dd8
Crstn <Crstn@192.168.1.36>
2013-05-18 11:10:51 +0100

    refactoring

--- src/PalindromeAnalyzer.java
+++ src/PalindromeAnalyzer.java
@@ -1,18 +1,24 @@
 public class PalindromeAnalyzer {
     
     public Boolean isPalindrome(int number){
-        String string = Integer.toString(number);
-        int[] digits = new int[string.length()];
+        int[] digits = stringToArray(Integer.toString(number));
+        return palindromeComparator(digits);
+    }
 
-        for(int i = 0; i<string.length(); ++i){
-            digits[i] = Integer.parseInt(string.substring(i, i+1));
-        }
+    private Boolean palindromeComparator(int[] digits) {
         for(int i=0; i < digits.length/2; i++){
-            if(digits[i]!= digits[digits.length -i -1]){
-                return false;
-            }
+            if(digits[i]!= digits[digits.length -i -1]) return false;
         }
         return true;
+    }
+
+    private int[] stringToArray(String string) throws NumberFormatException {
+        int[] digits = new int[string.length()];
+        
+        for(int i = 0; i<string.length(); ++i)
+            digits[i] = Integer.parseInt(string.substring(i, i+1));
+        
+        return digits;
     }
     
 }
